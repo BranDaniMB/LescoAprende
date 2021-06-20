e32d8e8d99b7863475ca82db63c900c341db9a91
CharlesCF99 <carlosdanielcabezasfallas@gmail.com>
2021-06-20 17:41:36 -0600

    Detalles mamalones

---
+++ app/src/main/java/com/ucr/lescoaprende/PhraseDetails.kt
@@ -0,0 +1,11 @@
+package com.ucr.lescoaprende
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PhraseDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phrase_details)
+    }
+}
