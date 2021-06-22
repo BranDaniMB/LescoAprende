4ae1c250c259fd5a4f4af003abc232948c9b285b
MaríaRH <maria.ramirez@ucrso.info>
2021-06-21 22:28:29 -0600

    Diccionario

---
+++ app/src/main/java/com/ucr/lescoaprende/Dictionary.kt
@@ -0,0 +1,11 @@
+package com.ucr.lescoaprende
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dictionary : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dictionary)
+    }
+}
